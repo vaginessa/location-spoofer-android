26e7f58e600d79dfe7fb163a924c54e9ae7bb8b8
Emmanuel Corrales <emmanuelmcorrales@gmail.com>
2016-03-15 23:50:39 +0800

    Initial commit.

---
+++ app/src/main/java/com/emmanuelcorrales/locationspoofer/MainActivity.java
@@ -0,0 +1,88 @@
+package com.emmanuelcorrales.locationspoofer;
+
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LocationManager mLocationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button mockLocationBtn = (Button) findViewById(R.id.mock_location);
+        mockLocationBtn.setOnClickListener(this);
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mLocationManager.addTestProvider(
+                LocationManager.GPS_PROVIDER,
+                false,
+                false,
+                false,
+                false,
+                true,
+                true,
+                true,
+                0,
+                5
+        );
+        mLocationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
+    }
+
+    private void mockLocation(double latitude, double longitude) {
+        Location mockLocation = new Location(LocationManager.GPS_PROVIDER); // a string
+        mockLocation.setLatitude(latitude);  // double
+        mockLocation.setLongitude(longitude);
+        mockLocation.setTime(System.currentTimeMillis());
+        mockLocation.setTime(System.currentTimeMillis());
+        mockLocation.setAccuracy(1f);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+            mockLocation.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+        } else {
+        }
+
+        mLocationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, mockLocation);
+    }
+
+    private LocationManager getLocationManager() {
+        return (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText latitudeEt = (EditText) findViewById(R.id.latitude);
+        EditText longitudeEt = (EditText) findViewById(R.id.longitude);
+        String message;
+        if (validateEditText(latitudeEt) | validateEditText(longitudeEt)) {
+            double latitude = Double.valueOf(latitudeEt.getText().toString());
+            double longitude = Double.valueOf(longitudeEt.getText().toString());
+            mockLocation(latitude, longitude);
+            message = "The location has been changed to (" + latitude + "," + longitude + ")";
+        } else {
+            message = "Spoofing the location has failed.";
+        }
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean validateEditText(EditText editText) {
+        if (editText == null) {
+            throw new IllegalArgumentException("Argument 'editText' cannot be null.");
+        }
+
+        if (editText.getText().toString().isEmpty()) {
+            editText.setError(getString(R.string.validation_required));
+            return false;
+        }
+        return true;
+    }
+}
