a39436997cb68d189b251578448e1a8ce7b5ebf4
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2016-11-16 00:19:09 +0800

    Implement spoofing on MapsActivity. Ask if changing location after a long click. Spoof the location if the user answered yes.

--- app/src/main/java/com/emmanuelcorrales/locationspoofer/MapsActivity.java
+++ app/src/main/java/com/emmanuelcorrales/locationspoofer/MapsActivity.java
@@ -1,22 +1,29 @@
 package com.emmanuelcorrales.locationspoofer;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
     private static final int REQUEST_PERMISSION_LOCATION = 7676;
 
     private GoogleMap mMap;
+    private LocationManager mLocationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +36,27 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        mLocationManager.addTestProvider(
+                LocationManager.GPS_PROVIDER,
+                false,
+                false,
+                false,
+                false,
+                true,
+                true,
+                true,
+                0,
+                5
+        );
+        mLocationManager.setTestProviderEnabled(LocationManager.GPS_PROVIDER, true);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.setOnMapLongClickListener(this);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                     == PackageManager.PERMISSION_GRANTED) {
@@ -51,7 +74,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
         switch (requestCode) {
             case REQUEST_PERMISSION_LOCATION:
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
@@ -62,5 +84,33 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 }
                 break;
         }
+    }
+
+    @Override
+    public void onMapLongClick(final LatLng latLng) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.dialog_mock_location)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mockLocation(latLng.latitude, latLng.longitude);
+                    }
+                }).setNegativeButton(android.R.string.no, null).show();
+    }
+
+    private void mockLocation(double latitude, double longitude) {
+        mockLocation(latitude, longitude, 50);
+    }
+
+    private void mockLocation(double latitude, double longitude, float accuracy) {
+        Location mockLocation = new Location(LocationManager.GPS_PROVIDER);
+        mockLocation.setLatitude(latitude);
+        mockLocation.setLongitude(longitude);
+        mockLocation.setAccuracy(accuracy);
+        mockLocation.setTime(System.currentTimeMillis());
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+            mockLocation.setElapsedRealtimeNanos(SystemClock.elapsedRealtimeNanos());
+        }
+        mLocationManager.setTestProviderLocation(LocationManager.GPS_PROVIDER, mockLocation);
     }
 }
