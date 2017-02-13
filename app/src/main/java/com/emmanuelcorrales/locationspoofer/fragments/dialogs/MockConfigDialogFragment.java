828b8649df9b32cb17be99dcb1233e50648908d8
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-02-13 12:08:23 +0800

    Prompt the user by showing a Toast if their device does not allow opening the Developer options.

--- app/src/main/java/com/emmanuelcorrales/locationspoofer/fragments/dialogs/MockConfigDialogFragment.java
+++ app/src/main/java/com/emmanuelcorrales/locationspoofer/fragments/dialogs/MockConfigDialogFragment.java
@@ -1,6 +1,7 @@
 package com.emmanuelcorrales.locationspoofer.fragments.dialogs;
 
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
@@ -8,6 +9,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.emmanuelcorrales.locationspoofer.R;
 
@@ -34,6 +37,11 @@ public class MockConfigDialogFragment extends DialogFragment
 
     @Override
     public void onClick(DialogInterface dialog, int which) {
-        startActivityForResult(new Intent(android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS), 0);
+        try {
+            startActivityForResult(new Intent(android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS), 0);
+        } catch (ActivityNotFoundException e) {
+            Log.e(TAG, "Cannot open Developer Options.");
+            Toast.makeText(getActivity(), "Cannot open Developer Options.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
