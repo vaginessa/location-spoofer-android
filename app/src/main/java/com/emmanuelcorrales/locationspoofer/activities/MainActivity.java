6445425b507a67c4d810e6a380cece21d3826cbc
Emmanuel Corrales <emmanuelcorrales22@gmail.com>
2017-03-11 01:14:12 +0800

    Return LocationCOnfigDialogFragment.

--- app/src/main/java/com/emmanuelcorrales/locationspoofer/activities/MainActivity.java
+++ app/src/main/java/com/emmanuelcorrales/locationspoofer/activities/MainActivity.java
@@ -7,14 +7,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.IntentSender;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -29,20 +27,11 @@ import com.emmanuelcorrales.android.utils.LocationUtils;
 import com.emmanuelcorrales.locationspoofer.LocationSpoofer;
 import com.emmanuelcorrales.locationspoofer.R;
 import com.emmanuelcorrales.locationspoofer.SpooferService;
+import com.emmanuelcorrales.locationspoofer.fragments.dialogs.LocationConfigDialogFragment;
 import com.emmanuelcorrales.locationspoofer.fragments.dialogs.MapHintDialogFragment;
 import com.emmanuelcorrales.locationspoofer.fragments.dialogs.MockConfigDialogFragment;
 import com.emmanuelcorrales.locationspoofer.fragments.dialogs.SpoofDialogFragment;
 import com.emmanuelcorrales.locationspoofer.utils.ConfigUtils;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResult;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -53,19 +42,17 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AnalyticsActivity implements ServiceConnection, OnMapReadyCallback,
         GoogleMap.OnMapLongClickListener, View.OnClickListener, GoogleMap.OnMarkerDragListener,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-        ResultCallback<LocationSettingsResult>, SpoofDialogFragment.OnSpoofListener {
+        SpoofDialogFragment.OnSpoofListener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     private static final int REQUEST_PERMISSION_LOCATION = 7676;
-    private static final int REQUEST_CHECK_LOCATION_SETTINGS = 43438;
 
     private GoogleMap mMap;
     private Marker mMarker;
     private DialogFragment mMockConfigDialog = new MockConfigDialogFragment();
+    private DialogFragment mLocationConfigDialog = new LocationConfigDialogFragment();
     private DialogFragment mMapHintDialog = new MapHintDialogFragment();
     private SpooferService mSpooferService;
-    private GoogleApiClient mGoogleApiClient;
 
     private BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
@@ -95,16 +82,11 @@ public class MainActivity extends AnalyticsActivity implements ServiceConnection
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
-
-        initGoogleApiClient();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.connect();
-        }
         Intent intent = new Intent(this, SpooferService.class);
         startService(intent);
         bindService(intent, this, BIND_AUTO_CREATE);
@@ -113,18 +95,12 @@ public class MainActivity extends AnalyticsActivity implements ServiceConnection
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (!LocationUtils.isGpnOn(this)) {
-            checkLocationSettings();
-        }
-    }
-
-    @Override
     protected void onResumeFragments() {
         super.onResumeFragments();
         if (!LocationSpoofer.canMockLocation(this)) {
             mMockConfigDialog.show(getSupportFragmentManager(), MockConfigDialogFragment.TAG);
+        } else if (!LocationUtils.isGpnOn(this) && !mLocationConfigDialog.isAdded()) {
+            mLocationConfigDialog.show(getSupportFragmentManager(), LocationConfigDialogFragment.TAG);
         } else if (ConfigUtils.isMapHintVisible(this)) {
             mMapHintDialog.show(getSupportFragmentManager(), MapHintDialogFragment.TAG);
         }
@@ -136,6 +112,10 @@ public class MainActivity extends AnalyticsActivity implements ServiceConnection
             mMockConfigDialog.dismiss();
         }
 
+        if (mLocationConfigDialog.isAdded()) {
+            mLocationConfigDialog.dismiss();
+        }
+
         if (mMapHintDialog.isAdded()) {
             mMapHintDialog.dismiss();
         }
@@ -145,7 +125,6 @@ public class MainActivity extends AnalyticsActivity implements ServiceConnection
 
     @Override
     protected void onStop() {
-        mGoogleApiClient.disconnect();
         LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
         unbindService(this);
         clearMarker();
@@ -192,49 +171,6 @@ public class MainActivity extends AnalyticsActivity implements ServiceConnection
             mMap.setMyLocationEnabled(true);
         }
         restoreMarkerPosition();
-    }
-
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Log.d(TAG, "onConnected");
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.d(TAG, "onConnectionCallback");
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        Log.d(TAG, "onConnectionFailed");
-    }
-
-    @Override
-    public void onResult(@NonNull LocationSettingsResult result) {
-        final Status status = result.getStatus();
-        switch (status.getStatusCode()) {
-            case LocationSettingsStatusCodes.SUCCESS:
-                // All location settings are satisfied. The client can
-                // initialize location requests here.
-                break;
-
-            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                // Location settings are not satisfied, but this can be fixed
-                // by showing the user a dialog.
-                try {
-                    // Show the dialog by calling startResolutionForResult(),
-                    // and check the result in onActivityResult().
-                    status.startResolutionForResult(this, REQUEST_CHECK_LOCATION_SETTINGS);
-                } catch (IntentSender.SendIntentException e) {
-                    // Ignore the error.
-                }
-                break;
-
-            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                // Location settings are not satisfied. However, we have no way
-                // to fix the settings so we won't show the dialog.
-                break;
-        }
     }
 
     @Override
@@ -331,34 +267,5 @@ public class MainActivity extends AnalyticsActivity implements ServiceConnection
             mMarker.remove();
             mMarker = null;
         }
-    }
-
-    private void checkLocationSettings() {
-        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
-                .addLocationRequest(createLocationRequest());
-
-        PendingResult<LocationSettingsResult> result =
-                LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient,
-                        builder.build());
-
-        result.setResultCallback(this);
-    }
-
-    private void initGoogleApiClient() {
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
-    }
-
-    protected LocationRequest createLocationRequest() {
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        return locationRequest;
     }
 }
